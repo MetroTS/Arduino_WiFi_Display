49577cf895a1db1090ba0dda7f001e07bb6b1bca
MetroTS <metrots8@github.com>
2026-05-04 12:04:56 +0200

    Arduino now successfully connects with wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <Arduino_LED_Matrix.h>
 #include "WiFiS3.h"
-#include "secrets.h"
+#include "secrets.h" // You'll want to make your own "secrets.h" file with 
+//                      #define SECRET_SSID "yourNetworkName"
+//                      #define SECRET_PASS "yourNetworkPasw"
 
 char ssid[] = SECRET_SSID;
 char pasw[] = SECRET_PASS;
@@ -10,6 +12,7 @@ int led = LED_BUILTIN;
 int status = WL_IDLE_STATUS;
 
 ArduinoLEDMatrix matrix;
+WiFiServer server(8888);
 
 uint8_t one[8][12] = {
   {1,0,0,0,0,0,0,0,0,0,0,0},
@@ -43,11 +46,15 @@ void setup() {
 
   Serial.println("\nConnected!");
   Serial.println(WiFi.localIP());
+  // wifi listener
+  server.begin();
+  Serial.println("Waiting for packets...");
+  // matrix init
   matrix.begin();
-
 }
 
 void loop() {
+  // check if wifi connection dropped and reastablish
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Wifi dropped... trying to reconnect.");
     WiFi.begin(ssid, pasw);
@@ -57,6 +64,5 @@ void loop() {
     }
     Serial.println("\nConnected!");
   }
-  matrix.renderBitmap(one, 8, 12);
   
 }
